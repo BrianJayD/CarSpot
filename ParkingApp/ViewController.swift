52dc1d58c6699e05d244131bfb35f9c2b7f2be82
BrianJayD <brian.jay1795@gmail.com>
2021-01-15 17:15:57 -0500

    Initial Commit

---
+++ ParkingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ParkingApp
+//
+//  Created by Brian Domingo on 2021-01-15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
