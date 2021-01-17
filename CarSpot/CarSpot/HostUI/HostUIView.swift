0afb67a3279aa22b4f72834821b20633bcf42d68
BrianJayD <brian.jay1795@gmail.com>
2021-01-17 14:02:59 -0500

    New renamed project with base code.

---
+++ CarSpot/CarSpot/HostUI/HostUIView.swift
@@ -0,0 +1,20 @@
+//
+//  HostUIView.swift
+//  CarSpot
+//
+//  Created by Brian Domingo on 2021-01-17.
+//
+
+import SwiftUI
+
+struct HostUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HostUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        HostUIView()
+    }
+}
---
+++ CarSpot/CarSpot/Views/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  CarSpot
+//
+//  Created by Brian Domingo on 2021-01-17.
+//
+
+import UIKit
+import SwiftUI
+
+class ViewController: UIViewController {
+    
+    let hostController = UIHostingController(rootView: HostUIView())
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        addChild(hostController)
+        hostController.view.translatesAutoresizingMaskIntoConstraints
+                                                          = false
+    
+        view.addSubview(hostController.view)
+    
+        hostController.view.centerXAnchor.constraint(
+                  equalTo: view.centerXAnchor).isActive = true
+        hostController.view.centerYAnchor.constraint(
+                  equalTo: view.centerYAnchor).isActive = true
+    
+        hostController.didMove(toParent: self)
+    }
+
+
+}
+
