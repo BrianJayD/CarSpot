0c157b3993b27187c469a9933bfc6303d38ee378
Daryl Dyck <77468055+daryldyck@users.noreply.github.com>
2021-01-18 11:40:22 -0500

    Color Theme Update.

---
+++ CarSpot/CarSpot/Models/Login.swift
@@ -0,0 +1,14 @@
+//
+//  Login.swift
+//  CarSpot
+//
+//  Created by Daryl Dyck on 2021-01-18.
+//
+
+import Foundation
+
+enum Login:String
+{
+    case REMEMBER_ME = "rememberMe"
+    case CURRENT_USER = "currentUser"
+}
--- CarSpot/CarSpot/Views/TabBarController.swift
+++ CarSpot/CarSpot/Views/TabBarController.swift
@@ -9,12 +9,34 @@ import UIKit
 
 class TabBarController: UITabBarController
 {
+    let defaults = UserDefaults.standard
 
     override func viewDidLoad()
     {
         super.viewDidLoad()
-
+        setup()
     }
 
-
+    override func viewWillAppear(_ animated: Bool)
+    {
+        navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
+    
+    
+    // setup nav buttons and other values
+    func setup()
+    {
+        self.navigationItem.titleView = UIImageView(image: UIImage(named: "logo_header"))
+        
+        self.navigationItem.leftBarButtonItem =
+            UIBarButtonItem(image: UIImage(named: "ic_logout"), style: .plain, target: self, action: #selector(logout(sender:)))
+        
+    }
+    
+    // logout of app
+    @objc func logout(sender: AnyObject)
+    {
+        defaults.set(nil, forKey: Login.CURRENT_USER.rawValue)
+        _ = navigationController?.popToRootViewController(animated: true)
+    }
 }
