906887bd17daa76e930b06c536151e55d759e63a
Daryl Dyck <77468055+daryldyck@users.noreply.github.com>
2021-01-18 10:42:30 -0500

    Added color values. Please use these values for everything to maintain consistency. (Colors are subject to be adjusted as we go along) ;-)

---
+++ CarSpot/CarSpot/Views/TabBarController.swift
@@ -0,0 +1,20 @@
+//
+//  TabBarController.swift
+//  CarSpot
+//
+//  Created by Daryl Dyck on 2021-01-18.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController
+{
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+
+    }
+
+
+}
