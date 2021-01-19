50385fe8b4e4a17a6ab9d9ba2ddbe7719015ca11
Daryl Dyck <77468055+daryldyck@users.noreply.github.com>
2021-01-19 16:19:43 -0500

    Map with current location fully working in Main page.

--- CarSpot/CarSpot/Views/SwiftUIs/MainSwiftUIView.swift
+++ CarSpot/CarSpot/Views/SwiftUIs/MainSwiftUIView.swift
@@ -6,17 +6,82 @@
 //
 
 import SwiftUI
+import MapKit
+import Combine
 
 struct MainSwiftUIView: View
 {
-    var body: some View {
-        Text("Main Page")
+    @ObservedObject private var locationManager = LocationManager()
+    @State private var cancellable: AnyCancellable?
+    @State private var region = MKCoordinateRegion()
+    @State var tracking: MapUserTrackingMode = .follow
+    @State private var currentLocation: [Location] = [Location]()
+
+    private func setCurrentLocation()
+    {
+        cancellable = locationManager.$location.sink { (location) in
+
+            self.region = MKCoordinateRegion(center: location?.coordinate ?? CLLocationCoordinate2D(),
+                                             latitudinalMeters: 500, longitudinalMeters: 500)
+
+            if(self.currentLocation.count == 0)
+            {
+                self.currentLocation.append(Location(coordinate: location?.coordinate ?? CLLocationCoordinate2D()))
+            }
+        }
     }
+
+    var body: some View
+    {
+        if (locationManager.location != nil)
+        {
+            Map(coordinateRegion: $region, interactionModes: .all, showsUserLocation: false, userTrackingMode: $tracking, annotationItems: currentLocation, annotationContent: { place in
+                MapAnnotation(coordinate: place.coordinate) {
+                    VStack {
+                        Text("Current Location")
+                            .padding(.leading, 10)
+                            .padding(.trailing, 10)
+                            .padding(.top, 5)
+                            .padding(.bottom, 5)
+                            .font(.system(size: 16.0))
+                            .foregroundColor(Color("textOnBackground"))
+                            .border(Color("cardOutline"), width: 1)
+                            .background(Color("cardBackground"))
+                            .cornerRadius(5)
+                            .shadow(color: .black, radius: 2, x: 1, y: 1)
+
+                        Spacer(minLength: 5)
+
+                        Image("ic_nav_map")
+                            .colorMultiply(Color("secondaryLight"))
+                            .shadow(color: .black, radius: 2, x: 1, y: 1)
+
+                        Spacer(minLength: 30)
+                    }
+                }
+            })
+                .onAppear {
+                    setCurrentLocation()
+                }
+                .ignoresSafeArea()
+        }
+        else
+        {
+            Text("Location user location...")
+        }
+    }
+}
+
+struct Location: Identifiable
+{
+    let id = UUID()
+    var coordinate: CLLocationCoordinate2D
 }
 
 struct MainSwiftUIView_Previews: PreviewProvider
 {
-    static var previews: some View {
+    static var previews: some View
+    {
         MainSwiftUIView()
     }
 }
