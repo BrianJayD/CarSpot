5191e4cd0ffb1920df2e3f2594d95c5c7fb00380
BrianJayD <brian.jay1795@gmail.com>
2021-01-20 02:39:05 -0500

    Changed code to use the new addSubSwiftUIView(swiftUIView:) from UIViewController extension.

--- CarSpot/CarSpot/Views/LoginViewController.swift
+++ CarSpot/CarSpot/Views/LoginViewController.swift
@@ -10,22 +10,11 @@ import SwiftUI
 
 class LoginViewController: UIViewController {
 
-    let hostController = UIHostingController(rootView: LoginSwiftUIView())
-
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        addChild(hostController)
-        hostController.view.translatesAutoresizingMaskIntoConstraints = false
-
-        view.addSubview(hostController.view)
-
-        hostController.view.centerXAnchor.constraint(
-            equalTo: view.centerXAnchor).isActive = true
-        hostController.view.centerYAnchor.constraint(
-            equalTo: view.centerYAnchor).isActive = true
-
-        hostController.didMove(toParent: self)
+        
+        self.addSubSwiftUIView(swiftUIView: LoginSwiftUIView())
+        
     }
     
 }
--- CarSpot/CarSpot/Views/MainViewController.swift
+++ CarSpot/CarSpot/Views/MainViewController.swift
@@ -14,16 +14,7 @@ class MainViewController: UIViewController
     {
         super.viewDidLoad()
 
-        setupSwiftUIView()
-    }
-
-    func setupSwiftUIView()
-    {
-        let hostController = UIHostingController(rootView: MainSwiftUIView())
-        self.addChild(hostController)
-        hostController.view.frame = self.view.frame
-        self.view.addSubview(hostController.view)
-        hostController.didMove(toParent: self)
+        addSubSwiftUIView(swiftUIView: MainSwiftUIView())
     }
 
 }
