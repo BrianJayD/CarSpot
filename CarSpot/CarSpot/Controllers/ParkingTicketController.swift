1518c437d57bb38bf4a73e18e92ef9769535a9f8
BrianJayD <brian.jay1795@gmail.com>
2021-01-22 13:44:46 -0500

    Fixed ParkingTicketController to match Ticket entity.

--- CarSpot/CarSpot/Controllers/ParkingTicketController.swift
+++ CarSpot/CarSpot/Controllers/ParkingTicketController.swift
@@ -12,69 +12,70 @@ import CoreData
 struct ParkingTicketController {
     
     private var moc:NSManagedObjectContext
-//    let fetchRequest = NSFetchRequest<ParkingTicket>(entityName: "ParkingTicket")
-//
-//    init() {
-//        self.moc = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
-//    }
+    let fetchRequest = NSFetchRequest<Ticket>(entityName: "Ticket")
+
+    init() {
+        self.moc = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    }
     
-//    func insertTicket(email:String, password:String, firstName:String, lastName:String, phoneNumber:Int) -> InsertStatus {
-//        do {
-//            let newAccount = NSEntityDescription.insertNewObject(forEntityName: "ParkingTicket", into: moc) as! ParkingTicket
-//
-//            newAccount.email = email
-//            newAccount.password = password
-//            newAccount.firstName = firstName
-//            newAccount.lastName = lastName
-//            newAccount.phoneNumber = Int64(phoneNumber)
-//
-//            try moc.save()
-//
-//            return InsertStatus.success
-//
-//        } catch let error as NSError {
-//            print(#function, "Error occured during insert")
-//            return InsertStatus.failed
-//        }
-//    }
-//
-//    func getAllAccounts() {
-//        do {
-//            let result = try moc.fetch(fetchRequest)
-//            let profiles = result as [Profile]
-//
-//            for profile in profiles {
-//                print("Email: \(profile.email) Passord: \(profile.password) Name: \(profile.firstName) \(profile.lastName)")
-//            }
-//
-//        } catch let error {
-//            print(#function, "Couldn't fetch records", error.localizedDescription)
-//        }
-//    }
-//
-//    func searchAccount(email:String) -> Profile? {
-//        let fetchRequest = NSFetchRequest<Profile>(entityName: "Profile")
-//
-//        //equivalent to a WHERE statement
-//        let predicate = NSPredicate(format: "email == %@", email)
-//        fetchRequest.predicate = predicate
-//
-//        do {
-//            let result = try moc.fetch(fetchRequest).first
-//            if result != nil {
-//                print(#function, "Matching account found wwith email \(email)")
-//
-//                let account = result as! Profile
-//                return account
-//
-//            }
-//        } catch let error {
-//            print(#function, error.localizedDescription)
-//        }
-//
-//        print(#function, "No account found with \(email)")
-//        return nil
-//    }
+    func insertTicket(email: String, buildingCode: String, noOfHours: Int, licensePlate: String, hostSuite: String, location: String) -> InsertStatus {
+        do {
+            let newTicket = NSEntityDescription.insertNewObject(forEntityName: "Ticket", into: moc) as! Ticket
+
+            newTicket.email = email
+            newTicket.buildingCode = buildingCode
+            newTicket.noOfHours = Int64(noOfHours)
+            newTicket.licensePlate = licensePlate
+            newTicket.hostSuite = hostSuite
+            newTicket.location = location
+
+            try moc.save()
+
+            return InsertStatus.success
+
+        } catch let error as NSError {
+            print(#function, "Error occured during insert")
+            return InsertStatus.failed
+        }
+    }
+
+    func getAllTicket() {
+        do {
+            let result = try moc.fetch(fetchRequest)
+            let tickets = result as [Ticket]
+
+            for ticket in tickets {
+                print(#function, "Email: \(ticket.email) License Plate: \(ticket.licensePlate) location: \(ticket.location)")
+            }
+
+        } catch let error {
+            print(#function, "Couldn't fetch records", error.localizedDescription)
+        }
+    }
+
+    func searchTicket(email:String) -> Ticket? {
+        let fetchRequest = NSFetchRequest<Ticket>(entityName: "Ticket")
+
+        //equivalent to a WHERE statement
+        let predicate = NSPredicate(format: "email == %@", email)
+        fetchRequest.predicate = predicate
+
+        do {
+            let result = try moc.fetch(fetchRequest).first
+            if result != nil {
+                print(#function, "Matching account found wwith email \(email)")
+
+                let ticket = result as! Ticket
+                return ticket
+
+            }
+        } catch let error {
+            print(#function, error.localizedDescription)
+        }
+
+        print(#function, "No account found with \(email)")
+        return nil
+    }
 
     
 }
