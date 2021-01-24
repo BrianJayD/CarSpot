9344277502995f1783e166dedec6ab92b7da5082
BrianJayD <brian.jay1795@gmail.com>
2021-01-24 06:42:12 -0500

    Prints all license plates when user logs in.

--- CarSpot/CarSpot/Views/MainViewController.swift
+++ CarSpot/CarSpot/Views/MainViewController.swift
@@ -10,11 +10,17 @@ import SwiftUI
 
 class MainViewController: UIViewController
 {
+    let licensePlateController = LicensePlateController()
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
-
+            
+        for plate in licensePlateController.getAllLicensePlatesForUser(email: UserDefaults.standard.string(forKey: Login.CURRENT_USER.rawValue)!) {
+            print(plate)
+        }
+        
+        
         UserDefaults.standard.set(true, forKey: Login.LOGGED_IN.rawValue)
         
         addSubSwiftUIView(swiftUIView: MainSwiftUIView())
