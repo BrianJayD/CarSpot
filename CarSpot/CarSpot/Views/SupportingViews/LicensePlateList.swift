629c14286af90fff0fd52bd77a1d95af2d28f01e
BrianJayD <brian.jay1795@gmail.com>
2021-01-25 06:05:28 -0500

    Completed Sign Up page with checks for each text field and completed list for License Plates.

---
+++ CarSpot/CarSpot/Views/SupportingViews/LicensePlateList.swift
@@ -0,0 +1,49 @@
+//
+//  LicensePlateList.swift
+//  CarSpot
+//
+//  Created by Brian Domingo on 2021-01-25.
+//
+
+import SwiftUI
+import UIKit
+
+struct LicensePlateList: View {
+    @State var plateNumbers:[PlateNumber]
+    
+    private func delete(with indexSet: IndexSet) {
+        indexSet.forEach { plateNumbers.remove(at: $0) }
+    }
+    
+    var body: some View {
+        
+        VStack {
+            Text("Your License Plates")
+                .font(.headline)
+            
+            List {
+                ForEach(plateNumbers) { plateNumber in
+                    HStack {
+                        Spacer()
+                        LicensePlateRow(plateNumber: plateNumber.plateNumber)
+                        Spacer()
+                    }
+                }.onDelete(perform: delete)
+                
+                
+            }
+        }
+        
+        
+    }
+}
+
+struct LicensePlateList_Previews: PreviewProvider {
+    static var previews: some View {
+        
+        LicensePlateList(plateNumbers: [
+                            PlateNumber(plateNumber: "BCA482", email: "b@b.com"),
+                            PlateNumber(plateNumber: "SICKVET", email: "vet@vet.com")
+        ])
+    }
+}
